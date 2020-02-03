a6386b83d2ee2be938f8b7cb2547dfc6cfda0689
galib45 <asadulgalibvat69@gmail.com>
2020-02-03 21:45:20 +0600

    changed the route for sitemap

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = flask.Flask(__name__)
 app.register_blueprint(portfolio)
 app.register_blueprint(donorfind)
 
-@app.route('/file/<path:pathOftheFile>')
-def send_file(pathOftheFile):
-	return flask.send_from_directory('', pathOftheFile)
+@app.route('/sitemap')
+def send_file():
+	return flask.send_from_directory('', 'sitemap.xml')
 
 if __name__=='__main__':
 	app.run()
