c428ed38ff21f872b57fa7c6fa89f7506dc68401
galib45 <asadulgalibvat69@gmail.com>
2021-04-12 19:39:31 +0600

    started coding for playlist

--- youtube/routes.py
+++ youtube/routes.py
@@ -1,6 +1,6 @@
 import flask
 from datetime import timedelta
-from pytube import YouTube
+from pytube import YouTube, Playlist
 from humanfriendly import format_size
 
 youtube = flask.Blueprint(
@@ -14,21 +14,28 @@ youtube = flask.Blueprint(
 @youtube.route('/', methods=['GET', 'POST'])
 def index():
 	if flask.request.method == 'GET':
-		return flask.render_template('youtube-index.html', data=None, format_size=format_size)
+		return flask.render_template('youtube-index.html')
 	else:
 		form = flask.request.form
 		url = form['url']
-		
-		yt = YouTube(url)
-		title = yt.title
-		duration = timedelta(seconds=yt.length)
-		
-		audios = yt.streams.filter(type='audio').order_by('filesize')
-		videos = yt.streams.filter(
-			type='video', 
-			custom_filter_functions=[lambda s: s.includes_audio_track]
-		).order_by('filesize')
 
-		data = (title, duration, audios, videos, yt)
-		
-		return flask.render_template('youtube-index.html', data=data, format_size=format_size)
+		if url.find('list=') != -1:
+			pl = Playlist(url)
+			files = [file for file in pl.videos]
+			playlist = (pl.title, len(files), files)
+			return flask.render_template('youtube-index.html', playlist=playlist, format_size=format_size)
+
+		else:
+			yt = YouTube(url)
+			title = yt.title
+			duration = timedelta(seconds=yt.length)
+			
+			audios = yt.streams.filter(type='audio').order_by('filesize')
+			videos = yt.streams.filter(
+				type='video', 
+				custom_filter_functions=[lambda s: s.includes_audio_track]
+			).order_by('filesize')
+
+			data = (title, duration, audios, videos, yt)
+			
+			return flask.render_template('youtube-index.html', data=data, format_size=format_size)
