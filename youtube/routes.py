c72971df82ab827d9ab97c8e0ebb83f526bc12cf
galib45 <asadulgalibvat69@gmail.com>
2021-04-14 19:20:47 +0600

    hudai

--- youtube/routes.py
+++ youtube/routes.py
@@ -39,3 +39,37 @@ def index():
 			data = (title, duration, audios, videos, yt)
 			
 			return flask.render_template('youtube-index.html', data=data, format_size=format_size)
+
+@youtube.route('/video/<string:id>')
+def videoById(id):
+	url = 'https://youtu.be/' + id
+	yt = YouTube(url)
+	title = yt.title
+	thumbnail = yt.thumbnail_url
+	duration = timedelta(seconds=yt.length).__str__()
+			
+	audios = yt.streams.filter(type='audio').order_by('filesize')
+	audios = [[audio.mime_type, audio.abr, format_size(audio.filesize), audio.url] for audio in audios]
+	
+	videos = yt.streams.filter(
+		type='video', 
+		custom_filter_functions=[lambda s: s.includes_audio_track]
+	).order_by('filesize')
+	videos = [[video.mime_type, video.resolution, format_size(video.filesize), video.url] for video in videos]
+
+	data = {
+		'title':title, 'duration':duration, 
+		'audios':audios, 'videos':videos, 
+		'thumbnail':thumbnail
+	}
+	return flask.jsonify(data)
+
+@youtube.route('/playlist/<string:id>')
+def playlistById(id):
+	url = 'https://www.youtube.com/playlist?list=' + id
+	pl = Playlist(url)
+	return flask.jsonify({'title':pl.title, 'videos':pl.video_urls})
+
+@youtube.route('/js')
+def js():
+	return flask.render_template('youtube-index.html', js=True)
