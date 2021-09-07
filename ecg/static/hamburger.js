0d71346d1258df0cebd6c57622207cd48782d836
galib45 <asadulgalibvat69@gmail.com>
2021-09-08 00:48:02 +0600

    modified the navbar

--- ecg/static/hamburger.js
+++ ecg/static/hamburger.js
@@ -2,24 +2,10 @@ let burger = document.querySelector('#hamburger');
 let list = document.querySelector('.nav-list');
 let bar = document.querySelector('.navbar-container');
 
-burger.addEventListener('click',e=>{
+burger.onclick = () => {
   if (window.getComputedStyle(list).getPropertyValue('display')=='none') {
     list.style.display='flex';
-    list.style.justifyContent = 'left';
-    bar.style.flexDirection = 'column'
+  } else {
+    list.style.display='none';
   }
-  else list.style.display='none';
-})
-
-window.addEventListener('resize', e=>{
-  if (window.innerWidth > 700) {
-    list.style.display='flex';
-    list.style.justifyContent = 'right';
-    bar.style.flexDirection = 'row';
-    bar.style.justifyContent =  'space-between';
-  }
-  else {
-    list.style.display = 'none';
-    bar.style.flexDirection = 'column'
-  }
-})
+}
